bc8f48fa03a5bb3531c7f2c8995de4489aba307d
Dabeer Farooqi <dabeer445@gmail.com>
2024-08-07 03:08:06 +0500

    updated API URL

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
-export const API_URL = 'https://1549cd01-8065-46e5-b169-c2a22da184b3-00-23ff59m2hrjzh.pike.replit.dev'
+// export const API_URL = 'https://1549cd01-8065-46e5-b169-c2a22da184b3-00-23ff59m2hrjzh.pike.replit.dev'
+export const API_URL = 'https://simply-big-back-end-levelfeed.replit.app'
